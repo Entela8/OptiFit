7c9af653c3695d9700682f608956b9f72e233716
entela8 <kyrasangster@gmail.com>
2023-10-01 00:22:53 +0200

    Search bar

--- app/src/main/java/com/example/optifit/MainActivity.kt
+++ app/src/main/java/com/example/optifit/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.optifit
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +18,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.optifit.ui.theme.OptiFItTheme
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var workouts: ListView
+    lateinit var listAdapter: ArrayAdapter<String>
+    lateinit var workoutsList: ArrayList<String>
+    lateinit var searchView: SearchView
+    val listView = findViewById<ListView>(R.id.idLVWorkouts)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -26,6 +38,62 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        workouts = findViewById(R.id.idLVWorkouts)
+        searchView = findViewById(R.id.searchWorkouts)
+
+        workoutsList = ArrayList()
+        workoutsList.add("Abdos")
+        workoutsList.add("Biceps")
+        workoutsList.add("Pectoraux")
+        workoutsList.add("Dos")
+        workoutsList.add("Jambes")
+        workoutsList.add("Fessiers")
+        workoutsList.add("Épaules")
+        workoutsList.add("Triceps")
+
+        workoutsList.add("Calisthénie")
+        workoutsList.add("Musculation")
+        workoutsList.add("Yoga")
+        workoutsList.add("Boxe")
+        workoutsList.add("Étirements")
+        workoutsList.add("Pilates")
+        workoutsList.add("CrossFit")
+
+        listAdapter = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_1,
+            workoutsList
+        )
+
+        workouts.adapter = listAdapter
+
+        searchView.setOnSearchClickListener {
+            listView.visibility = View.VISIBLE
+        }
+
+        searchView.setOnCloseListener {
+            listView.visibility = View.GONE
+            false
+        }
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                if (workoutsList.contains(query)) {
+                    listAdapter.filter.filter(query)
+                } else {
+                    Toast.makeText(this@MainActivity, "No Language found..", Toast.LENGTH_LONG)
+                        .show()
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                listAdapter.filter.filter(newText)
+                return false
+            }
+        })
     }
 }
 
